6e9e80fc42f30a4813f20fd024df0499d2879754
VitaliyStrunin <sthappy553@gmail.com>
2026-02-23 11:32:30 +0500

    config updated

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -9,7 +9,7 @@ CODE_LENGTH = 10
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = "postgres"
+DB_PORT = os.getenv("DB_HOST", 5432)
 DB_NAME = os.getenv("DB_NAME", "shortener_db")
 
 DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
